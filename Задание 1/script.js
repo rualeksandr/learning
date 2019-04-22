e438f58ed7c2057bbb52e8fbb6fa9cff54b8553f
RuAleksandr <RuAleksandr07@gmail.com>
2019-04-22 09:10:04 +0500

    Шестой коммит

--- Задание 1/script.js
+++ Задание 1/script.js
@@ -11,14 +11,6 @@ let appData = {
     saving: false
 };
 
-// let question1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-//     question2 = prompt('Во сколько обойдется?', ''),
-//     question3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-//     question4 = prompt('Во сколько обойдется?', '');
-
-// appData.expenses[question1] = question2;
-// appData.expenses[question3] = question4;
-
 for (let i = 0; i < 2; i++){
     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
         b = prompt('Во сколько обойдется?', '');
