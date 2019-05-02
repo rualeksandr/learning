93d0dc98fe5e50cb5b192d952e1aa88e91e3bc5c
RuAleksandr <RuAleksandr07@gmail.com>
2019-05-02 23:37:26 +0500

    18. получение элементов страницы html

---
+++ Сортировка чисел АЯ/tempCodeRunnerFile.js
@@ -0,0 +1,40 @@
+let mas = [7,5,33,550,-101,2,-99,-5,3.1,-3,0,3,0];
+
+console.log(sort(mas));
+
+function sort(arr){
+    let maxNum,
+        posMaxNum,
+        result = [];
+
+    function getMax(arr){     
+        maxNum = arr[0];
+        posMaxNum = 0;   
+
+        for(let i = 1; i < arr.length; i++){
+
+            if(maxNum > arr[i]){
+            }else if(maxNum < arr[i]){
+                maxNum = arr[i];
+                posMaxNum = i;
+            }else if(maxNum === arr[i]){
+                maxNum = arr[i];
+                posMaxNum = i;
+            }else{
+                console.log('Error: В массиве не число! (' + i + '. ' + arr[i] + ')');
+            }
+        } 
+        result.unshift(maxNum);
+        arr.splice(posMaxNum, 1);
+
+        if(arr.length > 1){
+            getMax(arr);
+        }else if(arr.length > 0){
+            result.unshift(arr[0]);
+        }else{
+            console.log('Error: В массиве нехватает элементов для сравнения!');
+        }
+    }
+    getMax(arr);
+    return result;
+}
--- Уроки/16. Динамическая типизация/type.js
+++ Уроки/16. Динамическая типизация/type.js
@@ -9,3 +9,44 @@
 // simbol
 // object - {}
 
+// - to String
+// 1) Старый спобоб String(), его лучше не использовать
+console.log(String(true));
+console.log(typeof(String(55)));
+// 2) Конкатенация. При сложении со строкой - получаем всегда строку
+console.log('' + true + 55 + null + undefined + {a:5});
+console.log( typeof( '' + true + 55 + null + undefined + {a:5} ) );
+
+// - to Number
+// 1) Старый спобоб Number(), его лучше не использовать
+console.log(Number(true));
+console.log(typeof(Number("555")));
+// 2) Унарный плюс
+console.log(5 + +"5");
+console.log(typeof( +"5" ));
+console.log(typeof( +null ));
+// 3) Методы, задача которых косвенно сводиться к преобразованию в число
+console.log(parseInt("558",8));
+console.log(typeof(parseInt("555",10)));
+
+// - to Boolean
+// false = 0, '', null, undefined, NaN
+// true = все остальное, включая пустые массивы, объекты
+
+// 1) способ преобразоватьть в boolean
+switcher = null;
+if(switcher){
+ console.log('Не выполниться. Null даст false.')
+}
+
+switcher = {};
+if(switcher){
+ console.log('Выполниться. Пустой объект даст true.')
+}
+
+// 2) способ - Boolean()
+console.log(typeof(Boolean("fff")));
+
+// 3) способ - !!
+console.log(typeof(!!"fff"));
+
