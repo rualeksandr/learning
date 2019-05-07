6cfcf20c4c58806ba61d454f224cfc08b143a4ba
RuAleksandr <RuAleksandr07@gmail.com>
2019-05-07 23:28:07 +0500

    7.05.19

--- Уроки/18. Получение элементов со страницы/script.js
+++ Уроки/18. Получение элементов со страницы/script.js
@@ -2,7 +2,8 @@ let box = document.getElementById('box'),
     btn = document.getElementsByTagName('button'),
     circle = document.getElementsByClassName('circle'),
     heart = document.querySelectorAll('.heart'),
-    oneHeart = document.querySelector('.heart');
+    oneHeart = document.querySelector('.heart'),
+    wrapper = document.querySelector('.wrapper');
 
 console.log(box);
 console.log(btn);
@@ -10,3 +11,36 @@ console.log(btn[0]);
 console.log(circle[2]);
 console.log(heart[1]);
 console.log(oneHeart);
+
+box.style.backgroundColor = "blue";
+btn[1].style.borderRadius = "100%";
+circle[0].style.backgroundColor = "red";
+circle[1].style.backgroundColor = "yellow";
+circle[2].style.backgroundColor = "green";
+
+// for(let i = 0; i < heart.length; i++){
+//     heart[i].style.backgroundColor = 'blue';
+// }
+
+// heart.forEach(function(item, i, hearts){
+//     item.style.backgroundColor = 'yellow';
+// });
+
+let div = document.createElement('div'),
+    text = document.createTextNode('Тест, текст..');
+
+div.classList.add('black');
+
+console.log(div);
+console.log(text);
+
+// document.body.appendChild(div);
+// wrapper.appendChild(div);
+document.body.insertBefore(div,circle[0]);
+document.body.removeChild(circle[1]);
+wrapper.removeChild(heart[1]);
+
+document.body.replaceChild(btn[1], circle[1]);
+
+// div.innerHTML = '<h3>Hello World!</h3>';
+div.textContent = ">Без верстки, более безопасно!";
